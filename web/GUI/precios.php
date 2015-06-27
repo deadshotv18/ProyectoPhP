2b75039a390711674d588bbf2d0778eddc316dce
martin villouta <deadshotv18@gmail.com>
2015-06-27 15:42:11 -0400

    agregada preciosGUI
    creada la pagina que muestra los precios por cada plan

---
+++ web/GUI/precios.php
@@ -0,0 +1,122 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Free Gym Website Template | Home :: w3layouts</title>
+<link href="../fw/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="../fw/css/style.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />-->
+<script src="js/jquery.min.js"></script>
+<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+</script>
+<!-- grid-slider -->
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="js/jquery.contentcarousel.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<!-- //grid-slider -->
+</head>
+<body>
+	<div class="menu" id="menu">
+	  <div class="container">
+		 
+		 <div class="h_menu4"><!-- start h_menu4 -->
+		   <a class="toggleMenu" href="#">Menu</a>
+			 <ul class="nav">
+			   <li><a href="index.html">Home</a></li>
+			   <li class="active" ><a href="pricing.html">Precios</a></li>
+			   <li><a href="login.php">Administracion	</a></li>
+			 </ul>
+			  <script type="text/javascript" src="js/nav.js"></script>
+		  </div><!-- end h_menu4 -->
+		 <div class="clear"></div>
+	  </div>
+	</div>
+	<!-- end menu -->
+	 <div class="main">
+	 	 <div class="container">
+			 <div class="col-md-4">
+		 		<ul class="price1">
+		 			<h4>Entrenamiento de 1 dia</h4>
+		 			<h3>Gratis</h3>
+		 			<ul class="price_list">
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Evaluacion Medica</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Acceso a maquinas</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">2 horas Limite</a></p>
+						<div class="clear"></div>
+					</ul>
+		 		</ul>
+			</div>
+			  <div class="col-md-4">
+		 		<ul class="price1">
+		 			<h4>Entrenamiento de 30 dias Basico</h4>
+		 			<h3>$17000</h3>
+		 			<ul class="price_list">
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Evaluacion Medica</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Acceso a maquinas</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">4 horas Limite</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Entrenador Personal</a></p>
+						<div class="clear"></div>
+					</ul>
+		 		</ul>
+			</div>
+			<div class="col-md-4">
+		 		<ul class="price1">
+		 			<h4>Entrenamiento de 30 dias Premium</h4>
+		 			<h3>$25000</h3>
+		 			<ul class="price_list">
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Evaluacion Medica</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Acceso a maquinas</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Sin tiempo Limite</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Entrenador Personal</a></p>
+						<p><img src="../images/tick.png" alt="" align="middle" width="16" height="14"/><a href="#">Alimentos Proteinicos Gratis</a></p>
+						<div class="clear"></div>
+					</ul>
+		 		</ul>
+			</div>
+		 </div>
+		<!-- end content-top -->
+		<div class="container">
+		    <div class="row content-middle">
+		      <!-- start content-middle -->
+				<div class="clear"></div>
+		   </div>
+		  <!-- end content-middle -->
+		 
+		    <div class="row content_middle_bottom">
+	<div class="clear"></div>
+     </div>
+		</div>
+		</div>
+		<div class="footer-bottom">
+		   <div class="container">
+		 	 <div class="row section group">
+				<div class="clear"></div>
+	  		  </div>
+		 	</div>
+		 </div>
+		 <div class="copyright">
+		  <div class="container">
+		    <div class="copy">
+		        <p>© 2014 Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a></p>
+		    </div>
+		    </div>
+		   <div class="clear"></div>
+		  </div>
+</body>
+</html>	
